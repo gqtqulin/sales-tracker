e480f24b3794d529932acd6a480e7c011e0db198
Oleg Gataulin <aaeqqk@MacBook-Air-Oleg.local>
2024-07-07 18:24:09 +0300

    tests upd

--- test/AlbumTracker.test.js
+++ test/AlbumTracker.test.js
@@ -16,7 +16,15 @@ describe("AlbumTracker", () => {
         }
     }
 
-    describe("deploy", () => {
+    describe("AlbumTracker", () => {
+        it("deploys tracker", async () => {
+            const { 
+                albumTracker, owner
+             } = await loadFixture(deployFixture)
+
+            expect(await albumTracker.currentIndex()).to.equal(0n)
+        })
+
         it("deploys album", async () => {
             const { 
                 albumTracker, owner
@@ -25,6 +33,7 @@ describe("AlbumTracker", () => {
             const title = "Чай вдвоем - Слава КПСС"
             const price = ethers.parseEther("0.00005")
             const createAlbumTx = await createAlbum(albumTracker, title, price)
+            //console.log(`createAlbumTx: ${JSON.stringify(createAlbumTx)}`)
     
             const expectedAlbumAddr = await precomputeAddress(albumTracker) // -- адрес дочернего контракта
             
@@ -34,7 +43,50 @@ describe("AlbumTracker", () => {
             expect(await album.price()).to.equal(price)
             expect(await album.title()).to.equal(title)
             expect(await album.purchared()).to.false
+
+            expect(await albumTracker.currentIndex()).to.equal(1n)
         })
+
+        it("create trigger payment event", async () => {
+            const {
+                albumTracker, owner, addr1
+            } = await loadFixture(deployFixture)
+
+            const title = "BACKGROUND I - remember."
+            const price = ethers.parseEther("0.0001")
+            const createAlbumTx = await createAlbum(albumTracker, title, price)
+
+            const expectedAlbumAddr = await precomputeAddress(albumTracker)
+
+            const album = await ethers.getContractAt("Album", expectedAlbumAddr)
+            await album.waitForDeployment()
+
+            const buyTxData = {
+                to: expectedAlbumAddr,
+                value: price
+            }
+
+            const buyTx = await addr1.sendTransaction(buyTxData)
+            await buyTx.wait()
+            
+            expect(await album.purchared()).to.true // альбом куплен
+            expect(
+                (await albumTracker.albums(0))
+            .state).to.equal(1) // в tracker в albums[0] стейт состояние - 1 (куплен)
+
+
+            // -- сделать проверку на событие
+
+
+            // -- сделать проверку на изменение баланса
+
+        })
+
+        // it("delivered trigger event", async () => {
+        //     const {
+        //         albumTracker, owner
+        //     } = await loadFixture(deployFixture)
+        // })
     })
 
 })
